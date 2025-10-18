e7662eac5b50559075b3e877a96db933fd8c9b66
beckfordluke14-design <beckfordluke14@gmail.com>
2025-10-18 13:38:52 -0400

    Update server.js

--- api/server.js
+++ api/server.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
 
       // Create a Checkout Session with Stripe
       const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
+        automatic_payment_methods: { enabled: true },
         line_items: items,
         mode: 'payment',
         shipping_address_collection: {
