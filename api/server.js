898c5d2e6b1e44637317c5ad455b7714c783560b
beckfordluke14-design <beckfordluke14@gmail.com>
2025-10-19 09:33:21 -0400

    Update server.js

--- api/server.js
+++ api/server.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
         line_items: items,
         mode: 'payment',
         shipping_address_collection: {
-          allowed_countries: ['US', 'CA', 'GB', 'AU', 'DO','UK'],
+          allowed_countries: ['US', 'CA', 'GB', 'AU', 'DO','GB'],
         },
         phone_number_collection: {
           enabled: true,
